79c1ea0a8a773cf2a9e0123ead12a434ae9f4c9d
Fang Zhao <zhaofang0627@gmail.com>
2020-12-07 21:14:30 +0400

    Update code

--- external/install_external.sh
+++ external/install_external.sh
@@ -1,5 +1,4 @@
 git clone https://github.com/shubhtuls/PerceptualSimilarity
 
-git clone https://github.com/hiroharu-kato/neural_renderer --branch v1.1.0
 cd neural_renderer
 python setup.py install
